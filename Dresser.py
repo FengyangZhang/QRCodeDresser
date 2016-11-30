1f4a4efb8b26507fa8ba2874bce470141815cb44
JohnfyBUPT <zhfy888@163.com>
2016-11-30 09:59:17 +0800

    changes

--- Dresser.py
+++ Dresser.py
@@ -5,52 +5,56 @@ import qrcode
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.misc
 
 def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:s:", ["help=", "image=", "sentence="])
     except getopt.GetoptError:
-        print('Please type in the image url and sentence -i <image> -s <sentence>')
+        print 'Please type in the image url and sentence -i <image> -s <sentence>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('Dresser.py -i <image> -s <sentence>')
+            print 'Dresser.py -i <image> -s <sentence>'
             sys.exit()
         elif opt in ("-i", "--image"):
             img_dress = np.array(Image.open(arg))
         elif opt in ("-s", "--sentence"):
-        	s = arg
+            s = arg
 
     qr = qrinit(s)
     
     img = qr.make_image()
     img_array = np.array(img)
-    shape = img_array.shape
-    print(img_array.dtype)
-    print(shape)
-    print(img_dress.shape)
-    if(shape[0] > img_dress.shape[0] or shape[1] > img_dress.shape[1]):
-        print('error: image size not agree!')
-        sys.exit(-1)
-    img_dress = img_dress[:, :, 0]
-    img_dress[img_dress <= 127] = 0
-    img_dress[img_dress > 127] = 1
-    img_dress = reduce(img_dress, shape)
+    img.save('QRCode.png', 'PNG')
 
-    img_produced = img_array * img_dress
-    img_produced[img_produced == 1] = True
-    img_produced[img_produced == 0] = False
-    print(img_produced.shape)
-    plt.imshow(img_produced)
-    plt.axis('off')
-    plt.show()
+
+    shape = img_array.shape
+    print shape
+    print img_dress.shape
+    if(shape[0] > img_dress.shape[0] or shape[1] > img_dress.shape[1]):
+        print 'error: image size not agree!'
+        sys.exit(-1)
+    mask = img_dress[:, :, 0] > 0.5
+    mask = reduce(mask, shape)
+    print mask.dtype
+    print mask.shape
+    scipy.misc.imsave('Mask.png', mask)
+    mask_mask = np.random.randn(shape[0] * shape[1]).reshape((shape[0], shape[1]))
+    mask_mask = mask_mask > 0
+    mask *= mask_mask
+    scipy.misc.imsave('Mask_masked.png', mask)
+    img_array *= mask
+    #img_produced = Image.fromarray(img_array.astype('uint8')*255)
+    #img_produced.save('Result.png', 'PNG')
+    scipy.misc.imsave('Result.png', img_array)   
 
 def qrinit(s):
     qr = qrcode.QRCode(
 	    version=None,
 	    error_correction=qrcode.constants.ERROR_CORRECT_H,
 	    box_size=10,
-	    border=4,
+	    border=1,
     )
     qr.add_data(s)
     qr.make(fit=True)
@@ -63,7 +67,7 @@ def reduce(image, shape):
     WW = image.shape[1]
     Hprop = int(HH / H)
     Wprop = int(WW / H)
-    image_reduced = np.zeros(shape)
+    image_reduced = np.zeros(shape, dtype = bool)
     for i in range(H):
         for j in range(W):
             image_reduced[i,j] = image[i*Hprop,j*Wprop]
